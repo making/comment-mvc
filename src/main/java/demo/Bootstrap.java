9e6b9cd319c629b30588275bbc408d85bc0abd4c
Toshiaki Maki <makingx@gmail.com>
2015-10-12 16:18:28 +0900

    fix host

--- src/main/java/demo/Bootstrap.java
+++ src/main/java/demo/Bootstrap.java
@@ -14,7 +14,7 @@ public class Bootstrap {
         String port = System.getProperty("server.port", System.getenv("PORT"));
         if (port == null || port.isEmpty()) port = "8080";
 
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create("http://localhost:" + port));
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create("http://0.0.0.0:" + port));
         WebappContext context = new WebappContext("GrizzlyContext", "");
         context.addServlet("JerseyContainer", new ServletContainer(new App())).addMapping("/*");
         context.addListener(new Listener());
